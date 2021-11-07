9487c413f30db32601e955aafc80ecfed051695e
skagml <namhui98@naver.com>
2021-11-07 12:49:47 +0900

    day01

---
+++ day01/day01/myinfo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "========자기소개========\n";
+	cout << "이름 : 김남희\n";
+	cout << "나이 : 24\n";
+	cout << "키 : 163\n";
+	cout << "전화번호 : 010-1234-5678\n";
+
+	return 0;
+}
